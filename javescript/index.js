27b3c7926e5d291f7d9c84b165eb0d17b7ea5acd
daniel <danielwzk>
2022-05-23 15:52:47 +0800

    fix : btn

--- javescript/index.js
+++ javescript/index.js
@@ -1,17 +1,19 @@
 const open = document.querySelector('.open-animation');
 setTimeout(() => {
   open.style.display = 'none';
-}, 5000);
+}, 4000);
 
 const projectsBtn = document.querySelectorAll('.projectsBtn');
 const aboutBtn = document.querySelectorAll('.aboutBtn');
 const projectPage = document.querySelector('.bgc-wrapper-1');
 const aboutPage = document.querySelector('.bgc-wrapper-2');
+const artTop = document.querySelector('.projects-top');
 
 projectsBtn.forEach((n) => {
   n.addEventListener('click', () => {
     aboutPage.classList.add('hidden');
-    window.scrollTo(0, 0);
+    console.log(artTop.offsetTop);
+    window.scrollTo(0, 732);
   });
 });
 
