dd6ac9bd85e79d36f76aa74a3924891907351562
Daniel Kai <danielkai@DanieldeMacBook-Air.local>
2024-03-16 20:37:08 +0800

    3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,101 @@ const Home = () => {
           </figure>
         </section>
         <section className='home_art1_show'>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </section>
+      </article>
+      <article className='home_art2'>
+        <section className='home_art2_title'>
+          <h1>APP UI</h1>
+          <h1>Projects</h1>
+        </section>
+        <section className='home_art2_main'>
+          <figure className='icon--into'>
+            <span></span>
+          </figure>
+          <ul className='card'>
+            <li className='card_info'>
+              <span></span>
+              <p>AI English UI Projects of the ponndy</p>
+            </li>
+            <li className='card_img'></li>
+          </ul>
+          <ul className='list'>
+            <li>
+              <p>01 Device Check Redesign</p>
+              <p>02 KIDS English</p>
+              <p>03 AI English</p>
+            </li>
+            <li>
+              <h2>04</h2>
+              <h2>Payment UI</h2>
+            </li>
+          </ul>
+        </section>
 
+
+      </article>
+      <article className='home_art3'>
+        <section className='home_art3_title'>
+          <h1>WebPage</h1>
+          <h1>Side Projects</h1>
+        </section>
+        <section className='home_art3_main'>
+          <ul className='card'>
+            <li className='card_info'>
+              <span></span>
+              <p>AI English UI Projects</p>
+            </li>
+            <li className='card_img'></li>
+          </ul>
+          <section className='img'>
+            <figure className='icon--into'>
+              <span></span>
+            </figure>
+          </section>
+          <ul className='list'>
+            <li>
+              <p>01 Device Check Redesign</p>
+              <p>02 KIDS English</p>
+              <p>03 AI English</p>
+            </li>
+          </ul>
+          <ul className='title'>
+            <li>
+              <span></span>
+              <h3>04</h3>
+              <h2>Payment UI</h2>
+            </li>
+          </ul>
+        </section>
+      </article>
+      <article className='home_art4'>
+        <section className='home_art4_title'>
+          <h1>Graphic</h1>
+          <h1>Design</h1>
+        </section>
+        <section className='home_art4_main'>
+          <ul className='card'>
+            <li className='card_info'>
+              <span></span>
+              <p>AI English UI Projects</p>
+            </li>
+            <li className='card_img'></li>
+          </ul>
+          <figure className='icon--into'>
+            <span></span>
+          </figure>
+          <ul className='list'>
+            <li>
+              <p>01 Device Check Redesign</p>
+              <p>02 KIDS English</p>
+              <p>03 AI English</p>
+            </li>
+          </ul>
         </section>
       </article>
     </main>
